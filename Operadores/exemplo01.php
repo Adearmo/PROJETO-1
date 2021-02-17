ab537446208a0a55b042bd9b5a97652040caf583
Adearmo <adearmoifg@gmail.com>
2021-02-17 18:30:02 -0300

    aula do dia 17-02-2021 as 18:29

---
+++ Operadores/exemplo01.php
@@ -0,0 +1,26 @@
+<?php 
+echo "<strong>Operadores de atribuicao. // =</strong>";
+echo "<br />";
+$nome = "TESTE";
+echo "$nome = 'teste'";
+echo "<br />";
+echo "<br />";
+echo "<strong>Operadores  de juncao.</strong>";
+echo "<br />";
+echo "<br />";
+echo "$nome .' mais Alguma coisa';// .";
+echo "<br />";
+echo $nome .' mais Alguma coisa';
+echo "<br />";
+echo "<br />";
+echo "<strong>Operadores  de atribuicao junto com o de juncao .</strong>";
+echo "<br />";
+echo "<br />";
+$nome.=" Treinamento";
+echo "<br />";
+echo"$nome.=' Treinamento'; // .=";
+echo "<br />";
+echo $nome;
+echo "<br />";
+echo "<br />";
+?>
---
+++ Operadores/exemplo05.php
@@ -0,0 +1,13 @@
+<?php
+$a=50;
+$b=35;
+$c=50;
+echo"resultado 1  o segundo e menor que o primeiro; 0 eles sao iguais; e -1 o segundo e maior que o Primeiro ";
+echo "<br>";
+var_dump($a<=>$b);
+echo "<br>";
+var_dump($a<=>$c);
+echo "<br>";
+var_dump($b<=>$a);
+
+?>
